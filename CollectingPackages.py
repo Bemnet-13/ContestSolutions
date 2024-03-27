1eebe88ef68fed41f21fe90b09fad850026b4369
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-03-28 02:45:14 +0300

    Corrected Collecting Packages question with a sorting approach

--- CollectingPackages.py
+++ CollectingPackages.py
@@ -1,36 +1,29 @@
-# Check if the package coordinates sorting is similar wither with x or y coordinate
-# Use a while loop to iterate through the packages
-# Use x and y coordinate pointers to locate the current position of the package
-# concatenate x * 'R' + y * 'U' to the path string
+t = int(input())
 
-tests = int(input())
-
-def path_finder(p, packages):
+for _ in range(t):
+    p = int(input())
+    coordinates = [[0, 0]]
+    moves = ""
+    result = True
 
     
-    x_sorted = sorted(packages, key = lambda coor: coor[0])
-    y_sorted = sorted(packages, key = lambda coor: coor[1])
-    if x_sorted[-1] != y_sorted[-1]:
+    for i in range(p):
+        x, y = list(map(int, input().split()))
+        coordinates.append([x, y])
+    coordinates.sort()
+
+    for i in range(1, p + 1):
+        if coordinates[i][0] == coordinates[i-1][0]:
+            moves += 'U' * (coordinates[i][1] - coordinates[i-1][1])
+        elif coordinates[i][1] == coordinates[i-1][1]:
+            moves += 'R' * (coordinates[i][0] - coordinates[i-1][0])
+        elif coordinates[i][0] > coordinates[i-1][0] and coordinates[i][1] > coordinates[i-1][1]:
+            moves += 'R' * (coordinates[i][0] - coordinates[i-1][0]) + 'U' * (coordinates[i][1] - coordinates[i-1][1])
+        else:
+            result = False
+            break
+    if not result:
         print('NO')
     else:
-        x = y = 0
-        path = ''
-        for i in range(len(y_sorted)):
-            if i == 0:
-                x = y_sorted[i][0]
-                y = y_sorted[i][1]
-            else:
-                x = y_sorted[i][0] - y_sorted[i-1][0]
-                y = y_sorted[i][1] - y_sorted[i -1][1]
-            path += x * 'R' + y * 'U'
         print('YES')
-        print(path)
-
-for _ in range(tests):
-    p = int(input())
-    packages = []
-    for _ in range(p):
-        xi , yi = map(int, input().split())
-        coor = tuple([xi,yi])
-        packages.append(coor)
-    path_finder(p = p, packages = packages)
+        print(moves)
