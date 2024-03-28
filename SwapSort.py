d0cd7eba14ae9f7b5f3d70b31049b45d0a8c9005
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-03-28 03:07:58 +0300

    Solved Swap Sort question using sorting

--- SwapSort.py
+++ SwapSort.py
@@ -1,26 +1,13 @@
 n = int(input())
-arr = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
-i = 0
-res = []
 
-while i < len(arr):     
-    num = max(arr)
-    for j in range(i, n):
-        if arr[i] > arr[j]:
-            num = min(num, arr[j])
-    if num != max(arr):
-        k = arr.index(num)
-        tup = i, k
-        arr[i], arr[k] = arr[k], arr[i]
-        res.append(tup)
-        i += 1
-    else:
-        i += 1
+print(n)
 
-if len(res) == 0:
-    print(0)
-else:
-    print(len(res))
-    for id1, id2 in res:
-        print(f"{id1} {id2}")
+for i in range(n):
+    pos = i
+    for j in range(i + 1, n):
+        if a[j] < a[pos]:
+            pos = j
+    print(i, pos)
+    a[i], a[pos] = a[pos], a[i]
